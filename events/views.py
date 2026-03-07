35ddff69ec846a7a9bb24979a71d5aa9d564d9d9
EC2 Default User <ec2-user@ip-172-31-81-234.ec2.internal>
2026-03-07 10:50:06 +0000

    validation

--- events/views.py
+++ events/views.py
@@ -20,16 +20,36 @@ def register_view(request): # pylint: disable=missing-function-docstring
     return render(request, "events/register.html", {"form": form})
 
 
-def login_view(request): # pylint: disable=missing-function-docstring
+# def login_view(request): # pylint: disable=missing-function-docstring
+#     if request.method == "POST":
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect("events")
+#     else:
+#         initial_data = {"username": "", "password": ""}
+#         form = AuthenticationForm(initial=initial_data)
+#     return render(request, "events/login.html", {"form": form})
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
+
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect("events")
+
     else:
-        initial_data = {"username": "", "password": ""}
-        form = AuthenticationForm(initial=initial_data)
+        form = AuthenticationForm()
+
     return render(request, "events/login.html", {"form": form})
 
 
